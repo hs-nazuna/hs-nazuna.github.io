049c5599a97389d363285635bb92187768108a9e
tsukasa_diary <tsukasadiary@gmail.com>
2017-06-26 13:55:36 +0900

    add menu.js

---
+++ fbs-menu.js
@@ -0,0 +1,15 @@
+menu = " ";
+menu += "<div id='contents'>";
+menu += "<h2 class='no_border'>もくじ</h2>";
+menu += "<ol>"
+menu += "<li><a href='index.html'>ページ概要</a></li>"
+menu += "<li>ZDD</li>"
+menu += "<li>フロンティア法</li>"
+menu += "<li>TdZddの初歩</li>"
+menu += "<li>実践1 : ナップサック問題</li>"
+menu += "<li>グラフデータとフロンティアの管理</li>"
+menu += "<li>パスの列挙</li>"
+menu += "<li>実践2 : サイクルの列挙</li>"
+menu += "<li>実践3 : ハミルトン-パス・サイクルの列挙</li>"
+menu += "</ol>"
+menu += "</div>"

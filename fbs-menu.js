c1272211c005b368f2097fac05970a105293100a
tsukasa_diary <tsukasadiary@gmail.com>
2017-06-28 12:48:54 +0900

    modify

--- fbs-menu.js
+++ fbs-menu.js
@@ -2,7 +2,7 @@ menu = " ";
 menu += "<div id='contents'>";
 menu += "<h2 class='no_border'>もくじ</h2>";
 menu += "<ol>"
-menu += "<li><a href='index.html'>ページ概要</a></li>"
+menu += "<li><a href='index.html'>概要</a></li>"
 menu += "<li>TdZddの初歩</li>"
 menu += "<li>実践1 : ナップサック問題</li>"
 menu += "<li>グラフデータとフロンティアの管理</li>"

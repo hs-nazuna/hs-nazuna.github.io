76d8ed3f3dc7b66540455ff44ed25947a8798907
tsukasa_diary <tsukasadiary@gmail.com>
2017-06-28 13:38:46 +0900

    add first step

--- fbs-menu.js
+++ fbs-menu.js
@@ -3,7 +3,7 @@ menu += "<div id='contents'>";
 menu += "<h2 class='no_border'>もくじ</h2>";
 menu += "<ol>"
 menu += "<li><a href='index.html'>概要</a></li>"
-menu += "<li>TdZddの初歩</li>"
+menu += "<li><a href='first-step-tdzdd.html'>TdZddの初歩</a></li>"
 menu += "<li>実践1 : ナップサック問題</li>"
 menu += "<li>グラフデータとフロンティアの管理</li>"
 menu += "<li>連結部分グラフの列挙</li>"

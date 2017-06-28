e9f6869a970397ef4c3d9ede192f23dbc0e5cb7f
tsukasa_diary <tsukasadiary@gmail.com>
2017-06-28 17:11:56 +0900

    modify menu

--- links.js
+++
@@ -1,7 +0,0 @@
-links = " ";
-links += "<div id='contents'>";
-links += "<h2 class='no_border'>リンク</h2>";
-links += "<ul>"
-links += <li><a href='https://github.com/hs-nazuna' target='_blank'>鈴木のgithub</a></li>
-links += "</ul>"
-links += "</div>"

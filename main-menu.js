a6fdae994c377570740c41946d6b296285344a48
tsukasa_diary <tsukasadiary@gmail.com>
2017-06-28 16:54:38 +0900

    update

--- main-menu.js
+++ main-menu.js
@@ -1,7 +1,9 @@
 menu = " ";
 menu += "<div id='contents'>";
 menu += "<h2 class='no_border'>コンテンツ</h2>";
-menu += "<ol>"
+menu += "<ul>"
+menu += "<li><a href='index.html#profile'>プロフィール</a></li>"
+menu += "<li><a href='index.html#achievements'>業績</a></li>"
 menu += "<li><a href='tdzdd-manual/intro.html'>TdZddマニュアル（作成中）</a></li>"
-menu += "</ol>"
+menu += "</ul>"
 menu += "</div>"

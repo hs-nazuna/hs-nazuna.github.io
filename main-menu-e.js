69eba391012481f1fcb3043fe8c2bc696599baad
tsukasadiary <tsukasadiary@gmail.com>
2017-08-17 14:58:20 +0900

    add english page

---
+++ main-menu-e.js
@@ -0,0 +1,14 @@
+menu = " ";
+menu += "<div id='contents'>";
+menu += "<h2 class='no_border'>Contents</h2>";
+menu += "<ul>"
+menu += "<li><a href='index-e.html#profile'>Profile</a></li>"
+menu += "<li><a href='index-e.html#achievements'>Achievements</a></li>"
+menu += "<li><a href='tdzdd-manual/intro.html' target='_blank'>Manual for TdZdd</a></li>"
+menu += "</ul>"
+menu += "<h2 class='no_border'>Link</h2>";
+menu += "<ul>"
+menu += "<li><a href='https://art.ist.hokudai.ac.jp/' target='_blank'>Large Scale Knowledge Processing Laboratory</a></li>"
+menu += "<li><a href='https://github.com/hs-nazuna' target='_blank'>My github</a></li>"
+menu += "</ul>"
+menu += "</div>"
